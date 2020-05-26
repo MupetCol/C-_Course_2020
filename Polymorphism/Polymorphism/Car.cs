b261c0dffddee7cd42773e473356cb7270b38384
MupetCol <65467881+MupetCol@users.noreply.github.com>
2020-05-26 12:52:54 -0500

    CursoC#_IntroPolymorphism
    - Use of virtual and override - new. Differences between them.

---
+++ Polymorphism/Polymorphism/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class Car
+    {
+        protected string color { get; set; }
+        protected int HP { get; set; }
+
+        public Car()
+        {
+            color = "black";
+            HP = 10000000;
+        }
+
+        public Car(string myColor, int myHP)
+        {
+            color = myColor;
+            HP = myHP;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Color: {0} - HP: {1}",color,HP);
+        }
+
+        public virtual void Repair()
+        {
+            Console.WriteLine("Car was repaired!");
+        }
+    }
+}
