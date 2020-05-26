450f7d9069eb7c66a085ad4e124e46d6564ed183
MupetCol <65467881+MupetCol@users.noreply.github.com>
2020-05-26 12:42:42 -0500

    CursoC#_Inheritance
    - Inheritance
    - Timer and callback method
    - "base" use for initializing constructors of derived classes

---
+++ Inheritance/Inheritance/ImagePost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+
+    // ImagePost derives from Post and adds a property (imageURL) and two constructors
+    class ImagePost:Post
+    {
+
+        public string ImageURL { get; set; }
+        public ImagePost() { }
+
+        public ImagePost(string title, string imageURL, string sendByUserName, bool isPublic)
+        {
+            //The following properties and method is inherited from Post.
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUserName = sendByUserName;
+            this.IsPublic = isPublic;
+            // Property imageURL is a member of ImagePost, but not from Post.
+            this.ImageURL = imageURL;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - {3}", this.ID, this.Title, this.ImageURL, this.SendByUserName);
+        }
+
+    }
+}
---
+++ Inheritance/Inheritance/PostGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class PostGenerator
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("Thanks for the birthday wishes",true,"Daniel Muñoz");
+
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "httpss//images.com/shoes", "Daniel Muñoz", true);
+
+            VideoPost videoPost1 = new VideoPost("Super THUMBNAIL", "https://music.youtube.com/watch?v=_Xq2wK3dI4g&list=RDAMVM_Xq2wK3dI4g", "Daniel Muñoz", 10, true);
+
+            Console.WriteLine(post1.ToString());
+            Console.WriteLine(videoPost1.ToString());
+            Console.WriteLine(imagePost1.ToString());
+
+            videoPost1.Play();
+            Console.WriteLine("Press any key to stop the video");
+            Console.ReadKey();
+            videoPost1.Stop();
+
+            Console.ReadLine();
+        }
+    }
+}
