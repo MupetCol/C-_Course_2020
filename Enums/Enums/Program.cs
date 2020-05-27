e3ac4bb82f2ff2c0190e1f5181c8fb08a330f440
MupetCol <65467881+MupetCol@users.noreply.github.com>
2020-05-26 23:34:07 -0500

    CursoC#_Enums

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enums
+{
+    // Set of constances, immutable, placed at the namespace level can be used by the whole library
+    enum Day { Mo, Tu, We, Th, Fr, Sa, Su};
+    // We can reasign index, and it will keep counting from that number
+    enum Month { Jan = 1, Feb, March, April, May, June = 12, July, Aug, Sept, Oct, Nov, Dec };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day mo = Day.Mo;
+
+            Day a = Day.Fr;
+
+            Console.WriteLine(fr == a);
+
+            Console.WriteLine(Day.Mo);
+            Console.WriteLine((int)Day.Mo);
+
+            Console.WriteLine((int)Month.Feb);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Enums/Enums/bru.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enums
+{
+    class bru
+    {
+        // As it is kept in the library, their values don't have to be defined in each class
+        Month jun = Month.Oct;
+    }
+}
